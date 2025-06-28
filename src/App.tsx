f9f53bf11ebaea2a31b157d6379187f51458c654
LoganPickart <pickart.four@gmail.com>
2025-06-27 23:29:13 -0400

    got bootstrap working

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <p className="h1">Hi, I'm Logan</p>
+    <div className="d-flex justify-content-center align-items-center vh-100">
+      <div className="text-center">
+      <p className=" h1 ">Hi, I'm Logan</p>
+      </div>
     </div>
   );
 }
